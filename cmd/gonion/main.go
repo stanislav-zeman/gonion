2f3896985bd5acfc49ef50a018775000ee1070c7
Stanislav Zeman <zeman@standa.dev>
2025-03-23 22:36:52 +0100

    feat: wire up all the services and run processor in main

---
+++ cmd/gonion/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/stanislav-zeman/gonion/internal/config"
+	processor "github.com/stanislav-zeman/gonion/internal/procesor"
+	"github.com/stanislav-zeman/gonion/internal/templator"
+	"github.com/stanislav-zeman/gonion/internal/writer"
+	yaml "gopkg.in/yaml.v3"
+)
+
+var (
+	configPath      = flag.String("config", "config/example.yaml", "project structure configuration")
+	outputDirectory = flag.String("out", "example", "project structure output directory")
+)
+
+func main() {
+	flag.Parse()
+
+	log.Println("running gonion...")
+	err := runGonion()
+	if err != nil {
+		log.Print(err)
+		os.Exit(1)
+	}
+}
+
+func runGonion() error {
+	f, err := os.ReadFile(*configPath)
+	if err != nil {
+		return fmt.Errorf("failed reading config file: %w", err)
+	}
+
+	var conf config.Config
+	err = yaml.Unmarshal(f, &conf)
+	if err != nil {
+		return fmt.Errorf("failed unmarshalling config file: %w", err)
+	}
+
+	t, err := templator.New("assets")
+	if err != nil {
+		return fmt.Errorf("failed creating templator: %w", err)
+	}
+
+	w := writer.NewWriter(*outputDirectory)
+	p := processor.New(conf, t, w)
+
+	err = p.Run()
+	if err != nil {
+		return fmt.Errorf("failed running processor: %w", err)
+	}
+
+	return nil
+}
