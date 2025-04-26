800c54afb97e67998e4c7f2a8ca91f42f773001e
Stanislav Zeman <zeman@standa.dev>
2025-04-27 00:34:38 +0200

    chore: regenerate example

--- example/internal/management/domain/entity/stream.go
+++ example/internal/management/domain/entity/stream.go
@@ -1,7 +1,11 @@
 package entity
 
+import (
+    "github.com/stanislav-zeman/gonion/example/internal/management/domain/value"
+)
+
 type Stream struct {
     ID string
     Name string
-    State value_object.State
+    State value.State
 }
